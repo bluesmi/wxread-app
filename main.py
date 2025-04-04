4713a6ad0a8892d9cffcd9e3cbcfa51ab79c15a0
MorningStart <morning-start@foxmail.com>
2025-04-04 20:19:33 +0800

    feat(main): 优化代码结构并实现异步处理
    - 重构 main.py 文件，引入 asyncio 和 pathlib 模块
    - 新增 process_curl_path 函数，用于处理单个 curl 脚本
    - 新增 setup_logger 函数，用于配置日志记录
    - 实现多任务异步处理，提高执行效率
    - 更新 .gitignore 文件，忽略 config 目录

--- main.py
+++ main.py
@@ -1,35 +1,66 @@
 import asyncio  # 添加 asyncio 导入
 import configparser
+import datetime
+from pathlib import Path
 
 from loguru import logger
 
 from sdk import WxPusherNotifier, WXReadSDK
 
+
+async def process_curl_path(curl_path, read_num):
+    FILE_NAME = Path(curl_path).stem
+    pusher = WxPusherNotifier(WXPUSHER_SPT)
+
+    def onStart(msg):
+        logger.info(f"{FILE_NAME}---{msg}")
+
+    def onSuccess(msg):
+        logger.info(f"{FILE_NAME}---{msg}")
+
+    def onRefresh(msg):
+        logger.info(f"{FILE_NAME}---{msg}")
+
+    def onFail(msg):
+        logger.error(f"{FILE_NAME}---{msg}")
+
+    def onFinish(msg):
+        logger.info(f"{FILE_NAME}---{msg}")
+        pusher.push(f"🎉 {FILE_NAME} 阅读脚本已完成！")
+
+    wx = WXReadSDK.from_curl_bash(curl_path)
+    await wx.sync_run(
+        loop_num=read_num * 2,
+        onStart=onStart,
+        onSuccess=onSuccess,
+        onRefresh=onRefresh,
+        onFail=onFail,
+        onFinish=onFinish,
+    )
+
+
+def setup_logger():
+    today = datetime.date.today()
+    log_file = f"logs/{today}.log"
+    logger.add(log_file, rotation="1 day", retention="7 days", encoding="utf-8")
+
+
+async def main():
+    tasks = (process_curl_path(curl_path, READ_NUM) for curl_path in CURL_PATH_LIST)
+    # 修改为异步运行
+    await asyncio.gather(*tasks)
+
+
 if __name__ == "__main__":
-    CURL_PATH = "./curl_config.sh"
-    CONFIG_PATH = "config.ini"
+    # config 文件夹下所有.sh
+    CURL_PATH_LIST = Path("./config").glob("*.sh")
+    CONFIG_PATH = "./config/key.ini"
     READ_NUM = 60
+
+    setup_logger()
 
     config = configparser.ConfigParser()
     config.read(CONFIG_PATH)
     WXPUSHER_SPT = config.get("WXPUSHER", "SPT")
 
-    pusher = WxPusherNotifier(WXPUSHER_SPT)
-    wx = WXReadSDK.from_curl_bash(CURL_PATH)
-
-    def onFail(msg):
-        logger.error(msg)
-        raise Exception(msg)
-
-    def onFinish(msg):
-        logger.info(msg)
-        pusher.push(msg)
-
-    # 修改为异步运行
-    asyncio.run(
-        wx.sync_run(
-            loop_num=READ_NUM * 2,
-            onFail=onFail,
-            onFinish=onFinish,
-        )
-    )
+    asyncio.run(main())
